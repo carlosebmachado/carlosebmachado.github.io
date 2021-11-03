da5f15e264628f86a252c5cc3301ad81ea31792f
Carlos Eduardo de Borba Machado <carlosebmachado@outlook.com>
2021-11-03 12:44:45 -0300

    dynamic multilang

--- assets/js/multilang.js
+++ assets/js/multilang.js
@@ -1,63 +1,119 @@
 class Multilang {
 
   constructor(path, lang) {
-
-    function loadJSON(callback) {
-      var xobj = new XMLHttpRequest();
-      xobj.overrideMimeType('application/json');
-      xobj.open('GET', path, false);
-      xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == '200') {
-          callback(xobj.responseText);
-        }
-      };
-      xobj.send(null);
+    var auxLang = getCookie('lang');
+    if (auxLang !== null) {
+      this._lang = auxLang;
+    } else {
+      this._lang = lang;
+      setCookie('lang', lang, 365);
     }
-
-    var langData;
-    loadJSON(function (response) {
-      langData = JSON.parse(response);
-    });
-    
-    this._langData = langData;
-    this._lang = lang;
+    this._path = path;
+    this.setFullPath();
+    this._langData = this.getLangData();
     this.setLang();
   }
 
   setLang(lang) {
-    lang(lang);
-    this.setLang();
+    if (lang !== undefined) {
+      setCookie('lang', lang, 365);
+      window.location.reload();
+    } else {
+
+      var html = document.getElementsByTagName('html')[0];
+      html.lang = this._lang;
+
+      var head = document.getElementsByTagName('head')[0];
+      head.childNodes.forEach(element => {
+        if (element.tagName !== 'SCRIPT' && element.tagName !== 'STYLE' && element.tagName !== 'LINK') {
+          element.outerHTML = this.parse(element.outerHTML);
+        }
+      });
+
+      var elements = document.querySelectorAll('body *');
+      elements = [...elements];
+      elements = elements.reverse();
+      elements.forEach(element => {
+        var inner = element.innerHTML.trim();
+        if (inner.charAt(0) === '{') {
+          var end = 0;
+          for (var i = 0; i < inner.length; ++i) {
+            var c = inner.charAt(i);
+            if (c === '}') {
+              end = i;
+              break;
+            }
+          }
+          var key = inner.substr(1, end - 1);
+          var content;
+          try {
+            content = this._langData[key];
+          } catch (e) {
+            console.error(e);
+          }
+          element.innerHTML = content + inner.substr(end + 1, inner.length - end - 2);
+        }
+      });
+      
+    }
   }
 
-  setLang() {
-    var elements = document.querySelectorAll('body *');
-    elements.forEach(element => {
-      var inner = element.innerHTML.trim();
-      if (inner.charAt(0) === '{' && inner.charAt(inner.length - 1) === '}') {
-        var key = inner.substr(1, inner.length - 2);
+  parse(html) {
+    if (html === undefined) return;
+    var inner = html;
+    var newInner = '';
+    for (var i = 0; i < inner.length; ++i) {
+      var c = inner.charAt(i);
+      if (c === '{') {
+        var iniPos = i + 1;
+        while (i < inner.length && c !== '}') {
+          ++i;
+          c = inner.charAt(i);
+        }
+        var endPos = i;
+        var key = inner.substr(iniPos, endPos - iniPos);
+        var content;
         try {
-          element.innerHTML = this._langData[this._lang][key];
+          content = this._langData[key];
         } catch (e) {
           console.error(e);
         }
+        newInner += content;
+      } else {
+        newInner += c;
       }
+    }
+    return newInner;
+  }
+
+  getLangData() {
+    var langData;
+    this.loadJSON(function (response) {
+      langData = JSON.parse(response);
     });
+    if (langData === undefined || langData === '' || langData === null) {
+      this._lang = 'pt-br';
+      this.loadJSON(function (response) {
+        langData = JSON.parse(response);
+      });
+    }
+    return langData;
   }
 
-  set langData(langData) {
-    this._langData = langData;
+  setFullPath() {
+    this._fullPath = this._path + 'lang.' + this._lang + '.json';
   }
 
-  get langData() {
-    return this._langData;
-  }
-
-  set lang(lang) {
-    this._lang = lang;
-  }
-
-  get lang() {
-    return this._lang;
+  loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType('application/json');
+    xobj.open('GET', this._fullPath, false);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == '200') {
+        callback(xobj.responseText);
+      }
+    };
+    xobj.send(null);
   }
 
 }
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,78 +1,72 @@
+window.multilang;
+
 $(document).ready(function () {
-    var theme = getCookie('theme');
-    if (theme != '') {
-        if (theme == 'dark') {
-            setTheme('dark');
-        } else {
-            setTheme('light');
-        }
+  // language
+  window.multilang = new Multilang('data/lang/', 'pt-br');
+
+  // theme
+  var theme = getCookie('theme');
+  if (theme != '') {
+    if (theme == 'dark') {
+      setTheme('dark');
     } else {
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            setTheme('dark');
-        } else {
-            setTheme('light');
-        }
+      setTheme('light');
     }
+  } else {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }
+});
+
+$('#btn-lang').on('click', function () {
+  var lang = getCookie('lang');
+  if (lang === 'en-us') {
+    window.multilang.setLang('pt-br');
+  } else {
+    window.multilang.setLang('en-us');
+  }
 });
 
 $('#btn-theme').on('click', function () {
-    var link = $('#theme-link');
-    var url = String(link.attr('href'));
-    var start = '../';
-    if (!url.startsWith(start)) {
-        start = '';
-    }
-    if (url == start + 'assets/css/light-theme.css') {
-        setTheme('dark');
-    } else if (url == start + 'assets/css/dark-theme.css') {
-        setTheme('light');
-    }
+  var link = $('#theme-link');
+  var url = String(link.attr('href'));
+  var start = '../';
+  if (!url.startsWith(start)) {
+    start = '';
+  }
+  if (url == start + 'assets/css/light-theme.css') {
+    setTheme('dark');
+  } else if (url == start + 'assets/css/dark-theme.css') {
+    setTheme('light');
+  }
 });
 
-function setTheme(theme){
-    if (theme != 'dark' && theme != 'light') return;
-    var lbl = $('#btn-theme-label');
-    if (theme == 'dark') {
-        lbl.text('Light');
-    } else {
-        lbl.text('Dark');
-    }
-    var link = $('#theme-link');
-    var url = String(link.attr('href'));
-    var start = '../';
-    if (!url.startsWith(start)) {
-        start = '';
-    }
-    link.attr('href', start + 'assets/css/' + theme + '-theme.css');
-    setCookie('theme', theme, 10 * 365);
+function setTheme(theme) {
+  if (theme != 'dark' && theme != 'light') return;
+  var lbl = $('#btn-theme-label');
+  if (theme == 'dark') {
+    lbl.text('Light');
+  } else {
+    lbl.text('Dark');
+  }
+  var link = $('#theme-link');
+  var url = String(link.attr('href'));
+  var start = '../';
+  if (!url.startsWith(start)) {
+    start = '';
+  }
+  link.attr('href', start + 'assets/css/' + theme + '-theme.css');
+  setCookie('theme', theme, 365);
 }
 
-// Cookies
-function setCookie(name, value, days) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        var expires = '; expires=' + date.toGMTString();
-    }
-    else var expires = '';               
-
-    document.cookie = name + '=' + value + expires + '; path=/';
-}
-
-function getCookie(name) {
-    var nameEQ = name + '=';
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-function removeCookie(name) {
-    setCookie(name, '', -1);
-}
+// Wait for window load
+$(window).on('load', function () {
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");;
+});
 
 /*!
 * Start Bootstrap - Resume v6.0.3 (https://startbootstrap.com/theme/resume)
@@ -80,39 +74,39 @@ function removeCookie(name) {
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
 (function ($) {
-    "use strict"; // Start of use strict
+  "use strict"; // Start of use strict
 
-    // Smooth scrolling using anime.js
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-            this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length ?
-                target :
-                $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                anime({
-                    targets: 'html, body',
-                    scrollTop: target.offset().top,
-                    duration: 1000,
-                    easing: 'easeInOutExpo'
-                });
-                return false;
-            }
-        }
-    });
+  // Smooth scrolling using anime.js
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+      this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ?
+        target :
+        $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        anime({
+          targets: 'html, body',
+          scrollTop: target.offset().top,
+          duration: 1000,
+          easing: 'easeInOutExpo'
+        });
+        return false;
+      }
+    }
+  });
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").on('click', function () {
-        $(".navbar-collapse").collapse("hide");
-    });
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").on('click', function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav"
-    });
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#mainNav"
+  });
 
 })(jQuery); // End of use strict
